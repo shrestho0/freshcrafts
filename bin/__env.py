6d25abf4e552f111efb1b779997f285b2f6f1bc3
Shrestho <sadrilaminshrestho@gmail.com>
2024-08-13 17:52:15 +0600

    installer ++

--- bin/__env.py
+++ bin/__env.py
@@ -1,48 +1,58 @@
+from _utils.utils import find_java17
 
-
-# EXECUTABLE_RELATIVE_LOCATIONS = {
-#     "engine": "../engine/target/engine-0.0.1-SNAPSHOT.jar",
-#     "cockpit": "../cockpit/build", # run 
-# }
+# Note: domain part must be same as in the `../cockpit/.env` file
+SYSTEM_DOMAIN = "freshcrafts.local"
 
 # Note: Ports must be same as in the environment vars / properties files
 REQUIRED_OPEN_PORTS=[80,443, 9092,9093, 27017, 17017, 15432, 13306, 10000, 10001,]
+
 SYSTEMD_SERVICE_DIRECTORY = "/lib/systemd/system/" # for ubuntu based systems
+
 NGINX_DIRECTORY = "/etc/nginx"
 DATA_FILE_LOCATION="./installation-data.json"
+
+DOCKER_COMPOSE_COMMAND="docker compose up -d"
+DOCKER_COMPOSE_FILE="../docker-compose.yml"
+
 LOG_DIR = "../fc_logs/"
 LOG_FILE_NAME = "installer-log"
-
+JAVA_17_AVAILABLE,JAVA_17_DIR = find_java17()
 TEMP_DIR = "./fc_temp/"
 
+
+
 FC_SERVICES_INFO = {
-    "engine": {
-        "ENV_FILE": "../engine/src/main/resources/application.properties",
-        "SYSTEMD_SERVICE_NAME": "fc_engine",
-        "SYSTEMD_SERVICE_TEMPLATE": "./templates/fc_common.service.template",
-        "SYSTEMD_EXECUTABLE": "../engine/target/engine-0.0.1-SNAPSHOT.jar",
-        "SYSTEMD_EXEC_COMMAND": "java -jar ",
-    },
     "wiz_mysql": {
         "ENV_FILE": "../wiz_mysql/src/main/resources/application.properties",
+        "SRC_DIR": "../wiz_mysql",
         "SYSTEMD_SERVICE_NAME": "fc_wiz_mysql",
-        "SYSTEMD_SERVICE_TEMPLATE": "./templates/fc_common.service.template",
-        "SYSTEMD_EXECUTABLE": "../wiz_mysql/target/wiz_mysql-0.0.1-SNAPSHOT.jar",
+        "SYSTEMD_SERVICE_TEMPLATE_FILE": "./templates/fc_common.service.template",
+        "SYSTEMD_EXECUTABLE_FILE": "../wiz_mysql/target/wiz_mysql-0.0.1-SNAPSHOT.jar",
+        "SYSTEMD_EXEC_COMMAND": "java -jar ",
+    },
+    "engine": {
+        "ENV_FILE": "../engine/src/main/resources/application.properties",
+        "SRC_DIR": "../engine",
+        "SYSTEMD_SERVICE_NAME": "fc_engine",
+        "SYSTEMD_SERVICE_TEMPLATE_FILE": "./templates/fc_common.service.template",
+        "SYSTEMD_EXECUTABLE_FILE": "../engine/target/engine-0.0.1-SNAPSHOT.jar",
         "SYSTEMD_EXEC_COMMAND": "java -jar ",
     },
 
     "wiz_postgres": {
         "ENV_FILE": "../wiz_postgres/src/main/resources/application.properties",
+        "SRC_DIR": "../wiz_postgres",
         "SYSTEMD_SERVICE_NAME": "fc_wiz_postgres",
-        "SYSTEMD_SERVICE_TEMPLATE": "./templates/fc_common.service.template",
-        "SYSTEMD_EXECUTABLE": "../wiz_postgres/target/wiz_postgres-0.0.1-SNAPSHOT.jar",
+        "SYSTEMD_SERVICE_TEMPLATE_FILE": "./templates/fc_common.service.template",
+        "SYSTEMD_EXECUTABLE_FILE": "../wiz_postgres/target/wiz_postgres-0.0.1-SNAPSHOT.jar",
         "SYSTEMD_EXEC_COMMAND": "java -jar ",
     },
     "wiz_mongo": {
         "ENV_FILE": "../wiz_mongo/src/main/resources/application.properties",
+        "SRC_DIR": "../wiz_mongo",
         "SYSTEMD_SERVICE_NAME": "fc_wiz_mongo",
-        "SYSTEMD_SERVICE_TEMPLATE": "./templates/fc_common.service.template",
-        "SYSTEMD_EXECUTABLE": "../wiz_mongo/target/wiz_mongo-0.0.1-SNAPSHOT.jar",
+        "SYSTEMD_SERVICE_TEMPLATE_FILE": "./templates/fc_common.service.template",
+        "SYSTEMD_EXECUTABLE_FILE": "../wiz_mongo/target/wiz_mongo-0.0.1-SNAPSHOT.jar",
         "SYSTEMD_EXEC_COMMAND": "java -jar ",
     },
 
@@ -50,9 +60,11 @@ FC_SERVICES_INFO = {
     "cockpit": {
         "COCKPIT": True,
         "ENV_FILE": "../cockpit/.env",
+        "SRC_DIR": "../cockpit",
+        "NGINX_CONFIG_TEMPLATE_FILE": "./templates/cocpit_nginx.conf.template",
         "SYSTEMD_SERVICE_NAME": "fc_cockpit",
-        "SYSTEMD_SERVICE_TEMPLATE": "./templates/fc_common.service.template",
-        "SYSTEMD_BUILD_DIRECTORY": "../cockpit/build",
+        "SYSTEMD_SERVICE_TEMPLATE_FILE": "./templates/fc_common.service.template",
+        "SYSTEMD_BUILD_DIR": "../cockpit/build",
         "SYSTEMD_EXEC_COMMAND": "/usr/bin/node --env-file=\"{ENV}\" \"{BUILD_DIR}\"", 
         # PROTOCOL_HEADER=x-forwarded-proto HOST_HEADER=x-forwarded-host node --env-file="/mnt/github/tjf/freshcrafts/cockpit/.env" "/mnt/github/tjf/freshcrafts/cockpit/build"
     },
@@ -60,7 +72,37 @@ FC_SERVICES_INFO = {
 
     }
 
-
+SYSTEM_DEPS = [
+        {
+            "package": "docker",
+            "checker_command": "docker --version",
+        },
+        {
+            "package":"docker-compose",
+            "checker_command": "docker compose version",
+        },
+        {
+            "package":"nginx",
+            "checker_command": "nginx -v",
+        },
+        { "package": "java",
+            "checker_command": "java -version",
+            "required_version": "17",
+        },
+        {
+            "package": "mvn",
+            "checker_command": "mvn -v",
+        },
+        {
+            "package": "node",
+            "checker_command": "node -v",
+            "required_version_min": "20.6",
+        },
+        {
+            "package": "npm",
+            "checker_command": "npm -v",
+        },
+]
 
 # FC_SERVICES_ENV_LOCATIONS = {
 #     "engine": {       
--- bin/_utils/utils.py
+++ bin/_utils/utils.py
@@ -1,4 +1,5 @@
 import os
+import subprocess
 import sys 
 def check_root_permission(console):
     """
@@ -8,3 +9,33 @@ def check_root_permission(console):
         console.print("You need to have root privileges to run this script.", style="bold red")
         sys.exit(1)
  
+def find_java17():
+    """
+    @description Finds the java 17 installation directory
+    @returns available:boolean, path:string
+    """
+    
+    java_home = os.environ.get('JAVA_HOME')
+    java_paths = [java_home] if java_home else []
+    java_paths.extend([
+        "/usr/lib/jvm/java-17-openjdk-amd64",
+        "/usr/lib/jvm/java-17-openjdk",
+        "/usr/java/jdk-17",
+        "/usr/lib/jvm/java-17-oracle",
+        #...more paths if required, later
+    ])
+
+    for path in java_paths:
+        java_bin = os.path.join(path, "bin", "java")
+        if os.path.exists(java_bin):
+            try:
+                version = subprocess.check_output([java_bin, "-version"], stderr=subprocess.STDOUT).decode()
+                if '17' in version:
+                    print(f"Java 17 found at: {path}")
+                    return True, path
+            except subprocess.CalledProcessError:
+                continue
+
+    return False, ""
+
+
